8af79b5c9f5be334ded477594744d683f246f648
coderwang110 <1131522675@qq.com>
2020-12-17 13:31:47 +0800

    初始化项目

---
+++ CCDemo/CCDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CCDemo
+//
+//  Created by dfc on 2020/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CCDemo/CCDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CCDemo
+//
+//  Created by dfc on 2020/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
